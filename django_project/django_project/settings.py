17d33e398c32cc6f2a09c40dd83cbcdf3089796a
github username <harr.hughes@gmail.com>
2020-10-03 19:29:46 +0100

    error solved

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
-    'cispy_forms'
+    'cispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
